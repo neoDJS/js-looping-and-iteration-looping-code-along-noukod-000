1462bc0eda81c81d12fc657104fa47720dc2bd4b
Dimanche Jhon Steeve <jsteeve@noukod.com>
2019-04-04 23:13:27 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const gifts = ['teddy bear', 'drone', 'doll'];
 
 function printBadges(gifts) {
     for (let i = 0; i < gifts.length; i++) {
-        console.log(`Welcome ${gifts[i]}! You are employee #${i}.`);
+        console.log(`Welcome ${gifts[i]}! You are employee #${i+1}.`);
     }
 
     return gifts;
