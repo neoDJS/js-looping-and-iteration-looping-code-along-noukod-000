42878f7ad6cc1f2f76c3ec0748526e7c7ba368c9
Dimanche Jhon Steeve <jsteeve@noukod.com>
2019-04-04 23:41:22 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Code your solutions in this file
+const gifts = ['teddy bear', 'drone', 'doll'];
+
+function printBadges(gifts) {
+    for (let i = 0; i < gifts.length; i++) {
+        console.log(`Welcome ${gifts[i]}! You are employee #${i+1}.`);
+    }
+
+    return gifts;
+}
+
+function maybeTrue() {
+    return Math.random() >= 0.5;
+}
+
+function tailsNeverFails(){
+  let count = 0;
+  while (maybeTrue()) {
+    count++;
+    console.log('You got ${count} tails in a row!');
+  }
+  return `You got ${count} tails in a row!`;
+}
