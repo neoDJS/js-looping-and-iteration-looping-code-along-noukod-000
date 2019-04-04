1d2ae2fc35d11c63127708e19947437228767e97
Dimanche Jhon Steeve <jsteeve@noukod.com>
2019-04-04 22:44:03 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,3 +4,7 @@ const gifts = ['teddy bear', 'drone', 'doll'];
 function wrapGift(gift) {
     console.log(`Wrapped ${gift} and added a bow!`);
 }
+
+for (let age = 30; age < 40; age++) {
+    console.log(`I'm ${age} years old. Happy birthday to me!`);
+}
