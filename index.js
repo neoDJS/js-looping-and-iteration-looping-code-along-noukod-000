7967ba373bbd4b217f1b624fe13ac6ce03196159
Dimanche Jhon Steeve <jsteeve@noukod.com>
2019-04-04 22:45:22 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 // Code your solutions in this file
 const gifts = ['teddy bear', 'drone', 'doll'];
-
-function wrapGift(gift) {
-    console.log(`Wrapped ${gift} and added a bow!`);
+ 
+function wrapGifts(gifts) {
+    for (let i = 0; i < gifts.length; i++) {
+        console.log(`Wrapped ${gifts[i]} and added a bow!`);
+    }
+ 
+    return gifts;
 }
-
-for (let age = 30; age < 40; age++) {
-    console.log(`I'm ${age} years old. Happy birthday to me!`);
-}
+ 
+wrapGifts(gifts);
